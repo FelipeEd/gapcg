25b8452359370e1713ed9ffd7eeb6b2156a2ca5b
Felipe Eduardo <44757639+FelipeEd@users.noreply.github.com>
2022-10-17 13:00:01 -0300

    add vertex to poligon

--- tp1.py
+++ tp1.py
@@ -2,14 +2,23 @@ from pygame.math import Vector2
 import numpy as np
 import pygame
 import pdb
+"""
+OBS: Para rodar o programa é necessario instalar o pacote numpy e pygame.
 
-# resolução da tela
-width, height = 640, 480
+O programa permite escolher o eixo de reflexao interativamente, inicialmente
+feito com um triangulo, mas basta adicionar pontos para ter novas figuras.
+
+Comandos:
+    Click com o botao esquerdo do mouse cria o eixo de reflexão e reflete o poligono.
+    Click com o botao direito adiciona um vertice no poligono.
+    Tecla r reseta o poligono.
+
+"""
+# Resolução da tela
+width, height = 640, 640
 max_x, max_y = width // 2, height // 2
 
-# converte um ponto cartesiano para as coordenadas do pygame
-
-
+# Converte um ponto cartesiano para as coordenadas do pygame
 def to_pygame(vetor):
     global width, height, max_x, max_y
 
@@ -20,16 +29,14 @@ def to_pygame(vetor):
 
     return vetor_trans[:-1].tolist()
 
-# converte um ponto nas coordenadas do pygame para cartesiano
-
-
+# Converte um poligono das coordenadas do pygame para cartesiano
 def poly_to_pygame(vertex):
     n_vertex = []
     for i in range(len(vertex)):
         n_vertex.append(to_pygame(vertex[i]))
     return n_vertex
 
-
+# Converte um ponto das coordenadas do pygame para cartesiano
 def to_cartesian(vetor):
     global width, height, max_x, max_y
 
@@ -39,6 +46,7 @@ def to_cartesian(vetor):
     return vetor_trans[:-1].tolist()
 
 
+# Reflete um ponto em relação k, com o angulo de k e o eixo x sendo alfa
 def reflect(alpha, vetor):
     ref = np.array([[np.cos(2 * alpha), np.sin(2 * alpha)],
                    [np.sin(2 * alpha), -np.cos(2 * alpha)]])
@@ -46,7 +54,7 @@ def reflect(alpha, vetor):
 
     return vetor_ref.tolist()
 
-
+# Reflete um poligono em relação a k, com o angulo de k e o eixo x sendo alfa
 def reflect_poly(alpha, vertex):
     new_vertex = []
     for i in range(len(vertex)):
@@ -60,7 +68,7 @@ def main():
 
     # pontos do poligono ------------------------------------------------------
 
-    poly_vertex = [[100, 200], [50, 50], [300, 100]]
+    poly_vertex = [[50, 200], [50, 50], [200, 50]]
 
     # ---------------------------------------------------------------------------
     origem = to_pygame([0, 0])
@@ -68,20 +76,31 @@ def main():
 
     running = True
     while running:
+        # Cor do fundo da tela
         screen.fill((30, 30, 45))
 
         for event in pygame.event.get():
-            # detecta click em fechar
+            # Detecta click em fechar
             if event.type == pygame.QUIT:
                 running = False
 
-            # detecta click para determinar eixo de reflexão
-            if event.type == pygame.MOUSEBUTTONUP:
+            # Detecta click para determinar eixo de reflexão
+            if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
                 pos = pygame.mouse.get_pos()
                 eixo = to_cartesian([pos[0], pos[1]])
                 rad = np.arctan2(eixo[1], eixo[0])
 
-        # desenha eixos cartesianos
+            # Acrescenta um vertice no poligono
+            if event.type == pygame.MOUSEBUTTONUP and event.button == 3:
+                pos = pygame.mouse.get_pos()
+                poly_vertex.append(to_cartesian([pos[0], pos[1]]))
+            
+            # Resetar o triangulo
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_r:
+                poly_vertex = [[100, 200], [50, 50], [300, 100]]
+                
+
+        # Desenha eixos cartesianos
         pygame.draw.line(screen, (200, 50, 50), to_pygame(
             [-max_x, 0]), to_pygame([max_x, 0]), 1)
         pygame.draw.line(screen, (200, 50, 50), to_pygame(
@@ -90,23 +109,19 @@ def main():
         pygame.draw.polygon(screen, (50, 50, 200),
                             tuple(poly_to_pygame(poly_vertex)))
 
-        # desenha eixo de reflexão
-        # pdb.set_trace()
+        # Desenha eixo de reflexão
         if eixo:
             eixo_vec = to_pygame(eixo) - Vector2(origem)
             eixo_vec = eixo_vec * width
             pygame.draw.line(screen, (0, 255, 0), origem -
                              eixo_vec, origem + eixo_vec, 2)
 
-            # desenha triângulo refletido
-            # a_ref = reflect(rad, a)
-            # b_ref = reflect(rad, b)
-            # c_ref = reflect(rad, c)
+            # Desenha triângulo refletido
             ref_poly = reflect_poly(rad, poly_vertex)
-            pygame.draw.polygon(screen, (80, 220, 20),
+            pygame.draw.polygon(screen, (200, 40, 180),
                                 tuple(poly_to_pygame(ref_poly)))
 
-        # exibe coordenadas do ponteiro do mouse
+        # Exibe coordenadas do ponteiro do mouse
         myFont = pygame.font.SysFont("Times New Roman", 16)
         pos = pygame.mouse.get_pos()
 
